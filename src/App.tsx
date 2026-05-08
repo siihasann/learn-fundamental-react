a7d9f9ca8c35b557af573a73fc3fbce1e406c24b
siihasann <mashasanr@gmail.com>
2026-05-08 15:23:17 +0700

    introduction conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Home from "./pages";
 
 function App() {
+  const value = false;
   return (
     <>
       <section>
         <Home />
+        {value ? "This is true" : "This is false"}
       </section>
     </>
   );
